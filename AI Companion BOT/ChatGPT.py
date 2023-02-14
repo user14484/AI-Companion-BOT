bbb74b2f1fefb64418c720455565f62719c5fd82
R1k3 <47985576+user14484@users.noreply.github.com>
2023-02-14 20:07:59 +0500

    Update ChatGPT.py

--- AI Companion BOT/ChatGPT.py
+++ AI Companion BOT/ChatGPT.py
@@ -3,39 +3,74 @@ import mtranslate
 import logging
 
 class ChatGPT:
-    logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
-    def __init__(self, api_key):
-        openai.api_key = api_key
+    # Добавляем логирование в файл
+    logging.basicConfig(level=logging.INFO, filename="logs.log",filemode="w")
 
-    def getAnswer(self, message, lang="ru", max_tokens=4000, temperature=0.5, engine_model="text-davinci-003"):
-        try:
-            # Считаем количество токенов
-            num_tokens = len(list(message))
-            # Если количество токенов превышает допустимое количество, то возращаем сообщение с ошибкой
-            if(num_tokens > max_tokens):
-                return mtranslate.translate("❌ You have exceeded the limit on the number of tokens. Please shorten your message.", lang, "auto")
+    # Список ключей Open AI
+    api_keys_list = []
+    # Колчичество удалённых ключей
+    removed_keys = 0
 
-            result = ""
+    # Функция инициализации объекта
+    # api_keys - список с ключами
+    def __init__(self, api_keys = []):
+        # Сохраняем список переданных ключей
+        self.api_keys_list = api_keys
+        # Задаём текущий ключ Open AI
+        openai.api_key = api_keys[0]
 
-            # Отправляем контекст на серверы OpenAI и получаем ответ
-            response = openai.Completion.create(
-                engine=engine_model,
-                prompt=message,
-                max_tokens=max_tokens,
-                n=1,
-                stop=None,
-                temperature=temperature,
-            )
-            result = response["choices"][0]["text"].strip()
+    # Функция отправки сообщения на сервера Open AI и получение ответа
+    # Описание параметров:
+    # message* - отправляемое сообщение
+    # lang - язык на котором нужно вернуть ответ (по умолчанию: "ru")
+    # max_tokens - максимальное количество токенов (по умолчанию: 1000)
+    # temperature - степень человечности ответа от 0 до 1 (по умолчанию: 0.7)
+    # engine_model - модель ChatGPT
+    def getAnswer(self, message, lang="ru", max_tokens=1000, temperature=0.7, engine_model="text-davinci-003"):
+        i = 0
+        errors = False
+        while(True):
+            try:
+                # Считаем количество токенов
+                num_tokens = len(list(message))
+                # Если количество токенов превышает допустимое количество, то возращаем сообщение с ошибкой
+                if(num_tokens > max_tokens):
+                    errors = True
+                    return {"message": mtranslate.translate("❌ You have exceeded the limit on the number of tokens. Please shorten your message.", lang, "auto"), "list_keys":self.api_keys_list, "attempts":i, "errors":errors}
+
+                # Отправляем текст на серверы OpenAI и получаем ответ
+                response = openai.Completion.create(
+                    engine=engine_model,
+                    prompt=message,
+                    max_tokens=max_tokens,
+                    temperature=temperature
+                )
+                # Формируем результат ответа
+                result = response["choices"][0]["text"].strip()
                 
 
-            # Если бот вернул пустой ответ
-            if(not result):
-                result = "❌ Sorry, the bot didn't return the result.";
+                # Если бот вернул пустой ответ
+                if(not result):
+                    errors = True
+                    result = "❌ Sorry, the bot didn't return the result.";
 
-            # Возращаем полученный и переводим его на нужный нам язык текст
-            return mtranslate.translate(result, lang, "auto")
-        except Exception as e:
-            logging.critical(f"{e}")
-            return mtranslate.translate("❌ An unknown error has occurred, try again!", lang, "auto")
+                # Возращаем результат работы
+                return {"message": mtranslate.translate(result, lang, "auto"), "list_keys":self.api_keys_list, "attempts":i, "errors":errors}
+            except Exception as e:
+                # Если на аккаунте Chat GPT закончилась квота, меням ключ
+                if "You exceeded your current quota" in str(e):
+                    if(self.RemoveKey()):
+                        return {"message": mtranslate.translate("❌ I'm sorry, but an unexpected error has occurred", lang, "auto"), "list_keys":self.api_keys_list, "attempts":i, "errors":errors}
+                i += 1
+    # Функция удаления ключа из списка
+    def RemoveKey(self):
+        print(len(self.api_keys_list))
+        if(len(self.api_keys_list) > 1):
+            self.api_keys_list.remove(self.api_keys_list[0])
+            openai.api_key = self.api_keys_list[0]
+            return False
+        elif(len(self.api_keys_list) > 0):
+            self.api_keys_list.remove(self.api_keys_list[0])
+            return True
+        return True
 
